83ea78c98a9c3562917cb574abd110d5e2611fd1
lilltfattiger <6698381@qq.com>
2022-05-15 23:47:34 -0700

    finish basic function for websocket communication

--- teamvoting/easyvoting/consumers.py
+++ teamvoting/easyvoting/consumers.py
@@ -1,39 +1,66 @@
 from channels.generic.websocket import WebsocketConsumer
 from channels.exceptions import StopConsumer
 from asgiref.sync import async_to_sync
+import json
 
-CONN_LIST = []
+CURRENT_USER = {}
+USER_CHANNEL_MAPPING={}
+USER_VOTING = {}
 
 
 class VotingConsumer(WebsocketConsumer):
     def websocket_connect(self, message):
-        print("someone connect")
         self.accept()
-        # CONN_LIST.append(self)
-        # self.send("来了啊 欢迎")
-        # group = self.scope['url_route']['kwargs'].get("group")
-        # async_to_sync(self.channel_layer.group_add)(group, self.channel_name)
+        group = self.scope['url_route']['kwargs'].get("group")
+        async_to_sync(self.channel_layer.group_add)(group, self.channel_name)
 
     def websocket_receive(self, message):
         text = message['text']
         print("got message -->", text)
-        # res = "{} SB".format(text)
-        # if text == "close":
-        #     self.close()
-        #     raise StopConsumer
-        #     return
-        # for conn in CONN_LIST:
-        #     conn.send(res)
-        # group = self.scope['url_route']['kwargs'].get("group")
-        # async_to_sync(self.channel_layer.group_send)(group, {"type": "xx.oo", 'message': message})
+        text_dict = json.loads(text)
+        group = self.scope['url_route']['kwargs'].get("group")
+        user = text_dict["user"]
+        content_type = text_dict["type"]
+        if content_type == "joinInfo":
+            CURRENT_USER[user] = 1
+            USER_CHANNEL_MAPPING[self.channel_name] = user
+            info = {
+                'msg_type': 1,
+                "user": user,
+                "alluser": CURRENT_USER,
+            }
+            async_to_sync(self.channel_layer.group_send)(group, {"type": "sync.info", 'message': info})
+        if content_type == "quitInfo":
+            if user in CURRENT_USER:
+                del CURRENT_USER[user]
+            info = {
+                'msg_type': 2,
+                "user": user,
+                "alluser": CURRENT_USER,
+            }
+            async_to_sync(self.channel_layer.group_send)(group, {"type": "sync.info", 'message': info})
+        elif content_type == "voteInfo":
+            info = {
+                'msg_type': 3,
+                "user": user,
+                "voting": text_dict["voting"]
+            }
+            async_to_sync(self.channel_layer.group_send)(group, {"type": "sync.info", 'message': info})
+        elif content_type == "resetInfo":
+            info = {
+                'msg_type': 4,
+                "user": user,
+            }
+            async_to_sync(self.channel_layer.group_send)(group, {"type": "sync.info", 'message': info})
 
-    def xx_oo(self, event):
-        text = event['message']['text']
-        self.send(text)
+    def sync_info(self, event):
+        self.send(json.dumps(event['message']))
 
     def websocket_disconnect(self, message):
-        # print("客户端主动断开链接")
-        # CONN_LIST.remove(self)
-        # group = self.scope['url_route']['kwargs'].get("group")
-        # async_to_sync(self.channel_layer.group_discard)(group, self.channel_name)
+        group = self.scope['url_route']['kwargs'].get("group")
+        async_to_sync(self.channel_layer.group_discard)(group, self.channel_name)
+        user = USER_CHANNEL_MAPPING[self.channel_name]
+        if user in CURRENT_USER:
+            del CURRENT_USER[user]
+        print("someone quit")
         raise StopConsumer
--- teamvoting/easyvoting/views.py
+++ teamvoting/easyvoting/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render, redirect
 
 
-
 # Create your views here.
 
 def index(request):
     user = request.GET.get('user')
     if not user:
-        return redirect('login/')
-    return render(request, 'index.html')
+        return redirect('/login/')
+    context = {
+        "user": user,
+        "group": 31415126
+    }
+    return render(request, 'index.html', context)
+
 
 def login(request):
     if request.method == 'GET':
@@ -16,10 +20,10 @@ def login(request):
     else:
         try:
             user = request.POST["user"]
+            print(user)
             if user and len(user) > 0:
-                return redirect('/index/',{"user":user})
+                return redirect(f"/index/?user={user}")
             else:
                 return render(request, 'login.html')
         except:
             return render(request, 'login.html')
-
